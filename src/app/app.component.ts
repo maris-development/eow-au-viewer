30e625f1cbfe3764afb5dd9e0978d0deb8e20e21
Brooke Smith <brooke.b.smith@csiro.au>
2019-10-28 12:46:02 +1100

    Angular-eow map rendering and more work to be done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,350 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild, AfterViewInit, OnInit} from '@angular/core';
+// import './css/styles.css';
+import debounce from 'lodash/debounce';
+import keyBy from 'lodash/keyBy';
+import groupBy from 'lodash/groupBy';
+import {HostListener} from '@angular/core';
+
+// import {
+//   Map,
+//   View,
+//   Overlay
+// } from 'ol';
+// import {
+//   fromLonLat
+// } from 'ol/proj';
+// import {
+//   Tile as TileLayer,
+//   Vector as VectorLayer
+// } from 'ol/layer';
+// import {
+//   OSM,
+//   Vector as VectorSource
+// } from 'ol/source';
+import Map from 'ol/Map';
+import XYZ from 'ol/source/XYZ';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import VectorSource from 'ol/source/Vector';
+import Overlay from 'ol/Overlay';
+import VectorLayer from 'ol/layer/Vector';
+import {fromLonLat} from 'ol/proj';
+
+import GeoJSON from 'ol/format/GeoJSON';
+import CircleStyle from 'ol/style/Circle';
+import {
+  Style,
+  Stroke,
+  Fill
+} from 'ol/style';
+import {
+  colors,
+  printDetails,
+  printStats,
+  calculateStats,
+  renderUsers,
+  recentMeasurements
+} from './utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('popup', {static: false}) popupEl: ElementRef;
   title = 'ng-eow';
+  map: Map;
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    this.map.setTarget('map');
+  }
+
+  ngOnInit() {
+
+// The WFS provided by EyeOnWater.org for Australia data
+    const WFS_URL = 'https://geoservice.maris.nl/wms/project/eyeonwater_australia?service=WFS'
+      + '&version=1.0.0&request=GetFeature&typeName=eow_australia&maxFeatures=5000&outputFormat=application%2Fjson';
+    const USER_SERVICE = 'https://www.eyeonwater.org/api/users';
+    const styleCache = {};
+    // this.map = null;
+    const allDataSource = new VectorSource({
+      format: new GeoJSON(),
+      url: WFS_URL
+    });
+
+// Fast datastructures to query the data
+    const userStore = {
+      users: [],
+      userById: {},
+      getUserById(userId) {
+        return this.userById[userId] || [];
+      }
+    };
+    const measurementStore = {
+      measurements: [],
+      measurementsById: {},
+      measurementsByOwner: {},
+      getByOwner(userId) {
+        return this.measurementsByOwner[userId] || [];
+      },
+      getById(id) {
+        return this.measurementsById[id] || [];
+      }
+
+    };
+// Get measurements from layer after it's done loading.
+    allDataSource.on('change', initialLoadMeasurements);
+
+    function initialLoadMeasurements(event) {
+      const source = event.target;
+      if (!source.loading) {
+        const features = allDataSource.getFeatures();
+        // Store the measurements in easy to access data structure
+        measurementStore.measurements = features;
+        measurementStore.measurementsById = keyBy(features, f => f.get('n_code'));
+        measurementStore.measurementsByOwner = groupBy(features, f => f.get('user_n_code'));
+
+        recentMeasurements(measurementStore.measurements);
+        allDataSource.un('change', initialLoadMeasurements);
+      }
+    }
+
+    const popup = new Overlay({
+      // element: this.popupEl.nativeElement,  // document.getElementById('popup'),
+      element: document.getElementById('popup'),
+      position: [0, 0],
+      autoPan: true,
+      autoPanMargin: 275,
+      positioning: 'bottom-center'
+    });
+
+// Style Features using ..... FU values (called for each feature on every render call)
+    const basicStyle = (feature, resolution) => {
+      const fuValue = feature.get('fu_value');
+      const styleKey = `${fuValue}_${resolution}`;
+      // Avoid some unnecessary computation
+      if (styleCache[styleKey]) {
+        return styleCache[styleKey];
+      }
+      feature.set('visible', true);
+      const styleOptions = {
+        image: new CircleStyle({
+          radius: this.map.getView().getZoom() * Math.log2(5),
+          stroke: new Stroke({
+            color: 'white'
+          }),
+          fill: new Fill({
+            color: colors[fuValue]
+          })
+        })
+      };
+
+      styleCache[styleKey] = new Style(styleOptions);
+      return styleCache[styleKey];
+    };
+
+    const dataLayer = new VectorLayer({
+      source: allDataSource,
+      style: basicStyle
+    });
+
+    dataLayer.on('change', debounce(({
+                                       target
+                                     }) => {
+      // Populate datalayer
+      // TODO - update
+      // document.querySelector('.sub-header-stats').innerHTML = printStats(calculateStats(target.getSource().getFeatures()), userStore);
+    }, 200));
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }),
+        dataLayer
+      ],
+      view: new View({
+        center: fromLonLat([133.07421121913038, 28.566680043403878]),
+        zoom: 2
+      }),
+      controls: []
+    });
+
+    async function loadUsers() {
+      const response = await window.fetch(USER_SERVICE);
+      const {
+        results: {
+          users
+        }
+      } = await response.json();
+      return users;
+    }
+
+    function clearFilter() {
+      dataLayer.setSource(allDataSource);
+      clearSelectedUser();
+      recentMeasurements(measurementStore.measurements);
+      this.map.getView().fit(dataLayer.getSource().getExtent(), {duration: 1300});
+      toggleFilterButton(false);
+    }
+
+    function showMeasurements(userId = null) {
+      const newSource = new VectorSource();
+      const selection = measurementStore.getByOwner(userId);
+      if (!selection.length) {
+        return false;
+      }
+      newSource.addFeatures(selection);
+
+      this.map.getView().fit(newSource.getExtent(), {
+        size: this.map.getSize(),
+        padding: [100, 100, 100, 100],
+        nearest: false,
+        duration: 1300
+      });
+      dataLayer.setSource(newSource);
+      recentMeasurements(selection);
+      return true;
+    }
+
+    function toggleFilterButton(state = false) {
+      const element = document.getElementById('clearFilterButton');
+      element.classList.toggle('hidden', !state);
+    }
+
+    function clearSelectedUser() {
+      document.querySelectorAll('.user-list .item').forEach(item => {
+        item.classList.remove('selectedUser', 'box-shadow');
+      });
+    }
+
+// Attach overlay and hide it
+    this.map.addOverlay(popup);
+    popup.setVisible(false);
+
+// // add some events
+//     popup.getElement().addEventListener('click', (event) => {
+//       if (event.target.matches('.close')) {
+//         popup.setVisible(false);
+//         popup.getElement().classList.remove('active');
+//       }
+//
+//       if (event.target.matches('.more-info-btn')) {
+//         const element = event.target.closest('.popup-item');
+//         element.classList.toggle('active');
+//       }
+//     }, false);
+//
+// Click events for panels
+    document.getElementById('clearFilterButton').addEventListener('click', (event) => {
+      clearFilter();
+    });
+
+/*
+    document.querySelectorAll('.pull-tab').forEach(i => i.addEventListener('click', (event: Event) => {
+      const element = event.target.closest('.panel');
+      element.classList.toggle('pulled');
+    }));
+
+    document.querySelector('.user-list').addEventListener('click', (event) => {
+      const element = event.target.closest('.item');
+      const userId = element.getAttribute('data-user');
+
+      if (showMeasurements(userId)) {
+        clearSelectedUser();
+        element.classList.add('selectedUser', 'box-shadow');
+        toggleFilterButton(true);
+      }
+    }, true);
+
+    document.querySelector('.measurement-list').addEventListener('click', (event) => {
+      const element = event.target.closest('.item');
+      if (!element) {
+        return;
+      }
+
+      const coordinate = element.getAttribute('data-coordinate').split(',');
+      const id = element.getAttribute('data-key');
+      const view = map.getView();
+      view.cancelAnimations();
+      view.animate({
+        center: coordinate,
+        zoom: 7,
+        duration: 1300
+      });
+      // clean up old popup and initilize some variables
+      popup.setVisible(false);
+      const popupElement = popup.getElement();
+      const content = popupElement.querySelector('.content');
+      const stats = popupElement.querySelector('.stats');
+      content.innerHTML = '';
+      popupElement.classList.remove('active');
+
+      const features = [measurementStore.getById(id)];
+
+      if (features.length) {
+        content.innerHTML = features.map(printDetails).join('');
+        stats.innerHTML = printStats(calculateStats(features), userStore);
+        popupElement.classList.add('active');
+
+        popup.setPosition(coordinate);
+      }
+    }, true);*/
+
+// Show popup with features at certain point on the map
+    this.map.on('click', (evt) => {
+      const {
+        pixel,
+        coordinate
+      } = evt;
+
+      // clean up old popup and initilize some variables
+      popup.setVisible(false);
+      const element = popup.getElement();
+      const content = element.querySelector('.content');
+      const stats = element.querySelector('.stats');
+      content.innerHTML = '';
+      element.classList.remove('active');
+
+      const features = [];
+
+      this.map.forEachFeatureAtPixel(pixel, (feature) => {
+        features.push(feature);
+      });
+
+      if (features.length) {
+        content.innerHTML = features.map(printDetails).join('');
+        stats.innerHTML = printStats(calculateStats(features), userStore);
+        element.classList.add('active');
+        popup.setPosition(coordinate);
+      }
+    });
+// Load users
+    loadUsers().then((users) => {
+      userStore.users = users;
+      userStore.userById = keyBy(userStore.users, 'id');
+      renderUsers(userStore.users);
+    });
+  }
+
+  // add some events
+ onClickPopup(event: any) {
+    // const popupEl = document.getElementById('popup');
+    if (event.target.matches('.close')) {
+      this.popupEl.nativeElement.setVisible(false);
+      // TODO - there exists an NG way of doing this
+      this.popupEl.nativeElement.getElement().classList.remove('active');
+    }
+
+    if (event.target.matches('.more-info-btn')) {
+      const element = event.target.closest('.popup-item');
+      element.classList.toggle('active');
+    }
+  }
 }
---
+++ src/app/utils.js
@@ -0,0 +1,173 @@
+import orderBy from 'lodash/orderBy'
+import {
+  DateTime
+} from 'luxon'
+
+// Load colors json from external file to keep things neat
+export const colors = require('./colors.json')
+
+export function getLargestAmount (collection) {
+  return Object.entries(collection).reduce((prev, [item, amount]) => {
+    if (amount > prev.amount) {
+      prev = {
+        item,
+        amount
+      }
+    }
+    return prev
+  }, {
+    item: null,
+    amount: -1
+  })
+}
+
+export function printDetails (feature) {
+  // Removed the geometry to avoid circular reference when serializing
+  let properties = Object.assign(feature.getProperties(), {
+    geometry: '*removed*'
+  })
+
+  let details = JSON.stringify(properties, null, 2)
+
+  return `
+  <div class="popup-item">
+  <div class="metadata-row"> 
+  <div class="image">
+  <img src="${properties.image}" />
+  </div>
+  <div class="metadata"> 
+  <div class="fu-preview"  style="background:${colors[properties.fu_value]}"></div><div class="more-info-btn"></div>
+  <div> FU value: ${properties.fu_value}</div>
+  <div> Date: ${properties.date_photo}</div>
+  <div> Device:  ${properties.device_model}</div>
+ </div>
+ </div>
+  <div class="raw-details">
+      <h4>Details<h4>
+      <pre>
+      ${details}
+      </pre>
+    </div>
+    </div>
+  `
+}
+
+export function calculateStats (features) {
+  let stats = {
+    iphones: 0,
+    androids: 0,
+    mostUsedDevice: null,
+    mostReportedFU: 0,
+    avgFU: 0,
+    mostActiveUser: null,
+    eowAu: 0,
+    eowGlobal: 0
+  }
+  let users = {}
+  let fuValues = {}
+  let devices = {}
+  // collect and aggregate
+  stats = features.reduce((stats, feature) => {
+    const properties = feature.getProperties()
+    // ~~ to coerce NaN/undefined to 0
+    // Only count users from EOW AU
+    if (properties.application === 'australia') {
+      users[properties.user_n_code] = ~~users[properties.user_n_code] + 1
+    }
+
+    fuValues[properties.fu_value] = ~~fuValues[properties.fu_value] + 1
+    devices[properties.device_model] = ~~devices[properties.device_model] + 1
+    stats[properties.device_platform === 'iOS' ? 'iphones' : 'androids'] += 1
+    stats[properties.application === 'australia' ? 'eowAu' : 'eowGlobal'] += 1
+    return stats
+  }, stats)
+
+  const mostReportedFU = getLargestAmount(fuValues)
+  const mostUsedDevice = getLargestAmount(devices)
+  const mostActiveUser = getLargestAmount(Object.assign({}, users, {
+    null: -2
+  }))
+
+  const avgFU = Object.entries(fuValues).reduce((prev, [fu, amount]) => {
+    return prev + (fu * amount)
+  }, 0) / features.length
+
+  return Object.assign({}, stats, {
+    mostReportedFU,
+    avgFU,
+    mostUsedDevice,
+    mostActiveUser
+  })
+}
+
+export function renderUsers (users, n = 10) {
+  const userList = orderBy(users, ['photo_count', 'points'], ['desc', 'desc']).slice(0, n).map(user => {
+    let itemTemplate = ` <li class="item" data-user="${user.id}">
+    <div>
+      <img  class="icon-thumb" src="https://eyeonwater.org/grfx/${user.icon}">
+    </div>
+    <div>
+      <div class="item-nickname">${user.nickname}</div>
+      <div class="item-photo-count">(${user.photo_count} photos)</div>
+      <div class="item-points">${user.points} points (level ${user.level})</div>
+    </div>
+  </li>`
+    return itemTemplate
+  })
+
+  document.querySelector('.user-list ul').innerHTML = userList.join('\n')
+}
+
+export function recentMeasurements (measurements, n = 20) {
+  const userList = orderBy(measurements, [(f) => (new Date(f.get('date_photo'))).getTime()], ['desc']).slice(0, n).map((measurement) => {
+    let prettyDate = DateTime.fromISO(measurement.get('date_photo')).toLocaleString(DateTime.DATE_FULL)
+
+    let itemTemplate = ` <li class="item measurement-item" data-coordinate="${measurement.getGeometry().getCoordinates()}" data-key="${measurement.get('n_code')}"><img src="https://eyeonwater.org/grfx/icons/small/${measurement.get('fu_value')}.png"> ${prettyDate}</li>`
+    return itemTemplate
+  })
+
+  document.querySelector('.measurement-list ul').innerHTML = userList.join('\n')
+}
+
+export function printStats (stats, userStore) {
+  return `
+    <div>
+      <ul>
+        <li>
+        <span class="stats-value">${stats.iphones}</span>
+        <span class="stats-label">iPhones</span>
+        </li>
+        <li>
+        <span class="stats-value">${stats.androids}</span>
+        <span class="stats-label">Androids</span>
+        </li>
+        <li>
+        <span class="stats-value">${stats.mostUsedDevice.item ? `${stats.mostUsedDevice.item}<span class="stat-extra">@${stats.mostUsedDevice.amount}</span>` : 'N/A'}</span>
+        <span class="stats-label">Most Used Device</span>
+        </li>
+        <li>
+        <span class="stats-value">${stats.mostReportedFU.item ? `${stats.mostReportedFU.item}<span class="stat-extra">@${stats.mostReportedFU.amount}</span>` : 'N/A'}</span>
+        <span class="stats-label">Most Reported FU</span>
+        </li>
+        <li>
+        <span class="stats-value">${Number(stats.avgFU).toFixed(2)}</span>
+        <span class="stats-label">Average FU</span>
+        </li>
+        <li>
+        
+        <span class="stats-value">${stats.mostActiveUser.item ? `${userStore.getUserById(stats.mostActiveUser.item).nickname}<span class="stat-extra">@${stats.mostActiveUser.amount}</span>` : 'N/A'}</span>
+        <span class="stats-label">Most Active User</span>
+        </li>
+        <li>
+        <span class="stats-value">${stats.eowAu}</span>
+        <span class="stats-label">EyeOnWater Australia</span>
+        </li>
+        <li>
+        <span class="stats-value">${stats.eowGlobal}</span>
+        <span class="stats-label">EyeOnWater: Worldwide</span>
+        </li>
+      </ul>
+      <div style="clear:both"></div>
+    </div>
+  `
+}
